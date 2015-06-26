8d3786d60cec498b552fe770a474b6e46afd6ea2
scensorECHO <xxartofwarxx@gmail.com>
2015-06-26 17:31:13 -0400

    Logs actions to file

--- extendwga.py
+++ extendwga.py
@@ -1,4 +1,4 @@
-# This application extends the WGA account for users with a provided credentials file, semicolon delimited
+# This application extends the WGA account for the given semicolon delimited credentials
 
 
 from selenium import webdriver
@@ -12,21 +12,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+##################################################
+################ LOG THREAD ######################
+##################################################
+
+from collections import deque
+import threading
+
+
+active = 1
+log = deque([])
+
+def logStackListener():
+	while(active):
+		if len(log)>0:
+			print(log.popleft())
+
+t = threading.Thread(target=logStackListener)
+t.start()
+
+##################################################
+############## END LOG THREAD ####################
+##################################################
+
 login = []
 email = []
+
+
+log.append('###Loading Selenium WebDriver###')
+
 url = "https://visitorportal.benteler.net:8443/sponsorportal/"
 # browser = webdriver.Firefox()
 browser = webdriver.PhantomJS('C:\phantomjs-2.0.0-windows\\bin\phantomjs.exe')
 
 with open('credentials','r') as c:
 	login = c.read().strip().split('\n')
+	log.append('Credentials read')
 
 with open('emails','r') as e:
 	emails = e.read().strip().split('\n')
+	log.append('Emails opened')
 
 def waitForPageById(element, seconds=15):
 	try:
-		wait = WebDriverWait(browser, seconds).until(
+		element = WebDriverWait(browser, seconds).until(
 			EC.presence_of_element_located((By.ID, element))
 		)
 		return 1;
@@ -35,7 +64,8 @@ def waitForPageById(element, seconds=15):
 
 def loadManagementPortal():
 
-	browser.get(url)
+	browser.get(url)	
+	log.append('Browser opened to WGA management portal')
 
 	# locate login and password forms
 	loginform = browser.find_element_by_id('loginpage.field.name')
@@ -49,7 +79,14 @@ def loadManagementPortal():
 	loginbutton = browser.find_element_by_id('loginpage.button.login')
 	loginbutton.click() 
 
-	return;
+	return 1;
+
+def queryUser(contactInfo):
+	# search for user
+	usernamefield = browser.find_element_by_id('searchUserNametxt')
+	usernamefield.send_keys(contactInfo[0])
+	usernamefield.send_keys(Keys.RETURN)
+	return 1;
 
 def addUser(contactInfo):
 	continue_link = browser.find_element_by_partial_link_text('Create Single Acco')
@@ -82,7 +119,7 @@ def addUser(contactInfo):
 	validityForm.send_keys('5')
 	timeZone.clear()
 	timeZone.send_keys('GMT')
-	time.sleep(.25) # page loads matches to GMT and can miss the space key, delayed to avoid mistype
+	time.sleep(.5)
 	timeZone.send_keys(' -05:00 EST')
 	# browser.executeScript("document.getElementsByName('guestUser.timezone').item(0).value ='GMT -05:00 EST');")
 	time.sleep(1)
@@ -97,17 +134,10 @@ def addUser(contactInfo):
 	# end account creation function
 	return 1;
 
-def queryUser(contactInfo):
-	# search for user
-	usernamefield = browser.find_element_by_id('searchUserNametxt')
-	usernamefield.send_keys(contactInfo[0])
-	usernamefield.send_keys(Keys.RETURN)
-	return;
-
 def findUserInPage(contactInfo):
 	usercheckbox = browser.find_element_by_xpath('//tr[@id="'+contactInfo[0]+'"]/td/input')
 	usercheckbox.click()
-	return;
+	return 1;
 
 def extendAccount():
 	# change account duration to 5 days
@@ -124,32 +154,47 @@ def extendAccount():
 	# confirm selection
 	confirmbtn = browser.find_element_by_id('OK')
 	confirmbtn.click()
-	return;
+	return 1;
 
 
-loadManagementPortal()
+if not loadManagementPortal():
+	active = 0
+log.append('Main portal logged in')
 
 time.sleep(3)
 
 for email in emails:
 
-	contactInfo = email.split(';')
+	if len(email) > 1:
+		contactInfo = email.split(';')
+	log.append('Contact info parsed for '+contactInfo[0])
+	if not isinstance(contactInfo, list):
+		log.append('Contact info incorrect')
+		continue
 
 	time.sleep(2)
 
-	queryUser(contactInfo)
+	if not queryUser(contactInfo):
+		continue
 
 	time.sleep(3)
 
 	try:
 		findUserInPage(contactInfo)
+		log.append('User found')
 	except:
-	 	addUser(contactInfo)
+	 	if not addUser(contactInfo):
+	 		continue
+	 	log.append('User added to system')
 		continue
 
 	time.sleep(1)
 
-	extendAccount()	
+	if not extendAccount():
+		continue
+	log.append('Account '+contactInfo[0]+' extended')
 
 # exit browser
+time.sleep(1)
+active = 0
 browser.quit()
