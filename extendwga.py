1ea6f97a75bf84939a8fdbdb4176ecc69c952a03
Tom <scensorECHO@users.noreply.github.com>
2015-12-02 11:44:57 -0500

    Updated to new portal structure

--- extendwga.py
+++ extendwga.py
@@ -9,10 +9,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
@@ -49,16 +47,19 @@ log.append('###Loading Selenium WebDriver###')
 url = "https://visitorportal.benteler.net:8443/sponsorportal/PortalSetup.action?portal=ba7b1560-7c73-11e5-88ac-005056997abd"
 manage = "https://visitorportal.benteler.net:8443/sponsorportal/LoginSubmit.action?from=LOGIN#manageAccountsList"
 
-# browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
 browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
 browser.set_window_size(1920,1080)
 # browser = webdriver.PhantomJS()
+browser.implicitly_wait(5)
 
-with open('/root/dl/xwga/credentials','r') as c:
+# '/root/dl/xwga/credentials'
+with open('credentials','r') as c:
 	login = c.read().strip().split('\n')
 	log.append('Credentials read')
 
-with open('/root/dl/xwga/emails','r') as e:
+# '/root/dl/xwga/emails'
+with open('emails','r') as e:
 	emails = e.read().strip().split('\n')
 	log.append('Emails opened')
 
@@ -93,20 +94,25 @@ def loadManagementPortal():
 	return 1;
 
 def queryUser(contactInfo):
-	# browser.get(manage)
-	# manageAccounts = browser.find_element_by_xpath('//a[@href="#manageAccountsList"]')
-	# manageAccounts = browser.find_element_by_xpath(
-	#	'//li[@class="ui-block-b"]/a[@class="ui_manage_accounts_button ui-btn ui-btn-inline ui-btn-up-a ui-btn-active"]')
+	time.sleep(5)
 	manageAccounts = browser.find_element_by_partial_link_text('Manage Accounts')
 	manageAccounts.click()
-	time.sleep(1)
+	time.sleep(2)
 	# search for user
 	# usernamefield = browser.find_elements_by_name('search')
 	# for u in usernamefield:
 	# 	print(u)
+	
+	# testing new username xpath
 	usernamefield = browser.find_element_by_xpath('//table[@class="search-container"]/tbody/tr/td/div/input')
-	usernamefield.click()
-	browser.save_screenshot("loggedin.png")
+	usernamefield.clear()
+
+	# try:
+	# 	username_clear= browser.find_element_by_xpath('//table[@class="search-container"]/tbody/tr/td/div/input/a')
+	# 	username_clear.click()
+	# except:
+	# 	log.append("No previous query to clear")
+
 	usernamefield.send_keys(contactInfo[0])
 	usernamefield.send_keys(Keys.RETURN)
 	return 1;
@@ -116,13 +122,15 @@ def addUser(contactInfo):
 	continue_link = browser.find_element_by_partial_link_text('Create Accounts')
 	continue_link.click()
 
-	if not waitForPageById('9d981c21-7c8e-11e5-be52-005056997abd8765b0e0-7c73-11e5-88ac-005056997abd'):
-		return 0; 
+	#if not waitForPageById('9d981c21-7c8e-11e5-be52-005056997abd8765b0e0-7c73-11e5-88ac-005056997abd'):
+	#	return 0; 
+	time.sleep(2)
 
 	# retrieve form elements
-	guesttype = browser.find_element_by_id("availableGuestTypes")
+	#guesttype = browser.find_element_by_id("availableGuestTypes")
+	guesttype = browser.find_element_by_xpath('//div[@id="availableGuestTypes"]/div/div/div/select')
 	forenameForm = browser.find_element_by_name("firstName")
-	surnameform = browser.find_element_by_name("lastName")
+	surnameForm = browser.find_element_by_name("lastName")
 	emailForm = browser.find_element_by_name("emailAddress")
 	phoneForm = browser.find_element_by_name("phoneNumber")
 	companyForm = browser.find_element_by_name("ui_optionaldata1_text_label")
@@ -130,11 +138,14 @@ def addUser(contactInfo):
 	locationForm = browser.find_element_by_name("ui_optionaldata4_text_label")
 	validityForm = browser.find_element_by_name("days")
 	locationList = browser.find_element_by_name("location")
-	# timeZone = browser.find_element_by_name("")
+	#locationList = browser.find_element_by_xpath('//span[@id="accessTimeContent"]/form/div/div/div/select')
+	ticketForm = browser.find_element_by_name("ui_optionaldata5_text_label")
 	
 	# set to 5 days
 	guesttype.click()
+	time.sleep(0.2)
 	guesttype.send_keys('5')
+	# browser.find_element_by_xpath('//option[@guest-type="9d981c21-7c8e-11e5-be52-005056997abd"]').click()
 	guesttype.send_keys(Keys.RETURN)
 
 	# fill out form
@@ -145,73 +156,96 @@ def addUser(contactInfo):
 	companyForm.send_keys(contactInfo[4])
 	contactForm.send_keys(contactInfo[5])
 	locationForm.send_keys(contactInfo[6])
+	ticketForm.send_keys(" ")
 	# set location dropdown
 	locationList.click()
-	locationList.send_keys("US/Michigan")
+	time.sleep(1)
+	# page refresh changes elements
+	locationList = browser.find_element_by_name("location")
+	locationList.click()
+	time.sleep(0.1)
+	locationList.send_keys("US/M")
 	locationList.send_keys(Keys.RETURN)
-	validityForm.clear()
-	validityForm.send_keys('5')
-	# timeZone.clear()
-	# timeZone.send_keys('GMT')
-	# time.sleep(.5)
-	# timeZone.send_keys(' -05:00 EST')
-	# browser.executeScript("document.getElementsByName('guestUser.timezone').item(0).value ='GMT -05:00 EST');")
-	# time.sleep(1)
+
 	# submit form
-	browser.find_element_by_id('createButton').click()
+	time.sleep(0.5)
 
-	# back to guest accounts page
-	if not waitForPageById('viewGuestAccountCancel'):
-		browser.get(manage)
-	browser.find_element_by_id('viewGuestAccountCancel').click()
+	return createAccount()
 
-	# end account creation function
-	return 1;
+
+
+def createAccount(attempts=0):	
+	try:
+		browser.find_element_by_id('createButton').click()
+		#notify user
+		time.sleep(0.2)
+		browser.find_element_by_partial_link_text('Notify').click()
+		#email user
+		time.sleep(0.2)
+		emailBox = browser.find_element_by_id('createKnownAccountNotifyFormEmail')
+		if not emailBox.get_attribute("checked"):
+			emailBox.click()
+		#confirm email
+		time.sleep(0.2)
+		browser.find_element_by_partial_link_text('Ok').click()
+		# end account creation function
+	except:
+		if(attempts>3):
+			log.append("An error extending the account has occurred")
+			return 0
+		else:
+			browser.find_element_by_partial_link_text("Manage Accounts").click()
+			time.sleep(2)
+			browser.find_element_by_partial_link_text("Create Accounts").click()
+			time.sleep(2)
+			createAccount(attempts+1)
+	return 1
 
 def findUserInPage(contactInfo):
 	# usercheckbox = browser.find_element_by_xpath('//tr[@id="'+contactInfo[0]+'"]/td/input')
 	try:
-		usercheckbox = browser.find_element_by_xpath('//tr[@class="ui-body-a"]/td/input')
-		usercheckbox.click()
+		searchedUser = browser.find_element_by_partial_link_text(contactInfo[0])
+		#searchedUser = browser.find_element_by_xpath('//table[@class="manage-accounts-table"]/tbody/tr/td[@title="'
+		#	+contactInfo[0]+'"/a')
+		searchedUser.click()
 	except:
-		notFound(contactInfo[0])
+		log.append('User '+contactInfo[0]+' not found in system')
 		return 0
 	return 1
 
 def extendAccount():
-	# change account duration to 5 days
+	# wait for page load
+	time.sleep(2)
 	editButton = browser.find_element_by_partial_link_text("Edit")
 	editButton.click()
 
-	# wait for page load
-	time.sleep(1)
+
+	# set Guest Type
+	guesttype = browser.find_element_by_xpath('//div[@id="availableGuestTypesEdit"]/div/div/div/select')
+	guesttype.click()
+	time.sleep(0.2)
+	guesttype.send_keys('5')
+	# browser.find_element_by_xpath('//option[@guest-type="9d981c21-7c8e-11e5-be52-005056997abd"]').click()
+	guesttype.send_keys(Keys.RETURN)
+	browser.find_element_by_xpath('//body').click()
 
 	# select 5 days from dropdown box
-	# dropdown = browser.find_element_by_xpath("//select[@id='timeProfileDropdown']/option[3]")
-	durationField = browser.find_element_by_name("days")
-	durationField.send_keys('5')
-	
-	guesttype = browser.find_element_by_id("availableGuestTypesEdit")
-	guesttype.click()
-	guesttype.send_keys('5')
-	guesttype.send_keys(Keys.RETURN)
+	time.sleep(1)
+	#durationField = browser.find_element_by_name("days")
+	#durationField.send_keys('5')
 
 	# confirm selection
-	confirmbtn = browser.find_element_by_id('Submit')
+	confirmbtn = browser.find_element_by_partial_link_text('Submit')
 	confirmbtn.click()
-	return 1;
+	return 1
 
-def notFound(e):
-	nfe = 'User '+e+' not found in system'
-	log.append(nfe)
-	print(nfe)
-
+## Main Application
 
 if not loadManagementPortal():
 	active = 0
 log.append('Main portal logged in')
 
-time.sleep(3)
+time.sleep(1)
 
 for email in emails:
 
@@ -222,7 +256,7 @@ for email in emails:
 		log.append('Contact info incorrect')
 		continue
 
-	time.sleep(2)
+	time.sleep(1)
 
 	if not queryUser(contactInfo):
 		continue
@@ -230,18 +264,18 @@ for email in emails:
 
 	time.sleep(3)
 
-	try:
-		findUserInPage(contactInfo)
-		log.append('User found')
-	except:
-	 	if not addUser(contactInfo):
+	if not findUserInPage(contactInfo):
+		if not addUser(contactInfo):
+			log.append('User addition failed')
 	 		continue
 	 	log.append('User added to system')
 		continue
+	log.append('User found')	
 
 	time.sleep(1)
 
 	if not extendAccount():
+		log.append('Account extension for '+contactInfo[0]+' failed')
 		continue
 	log.append('Account '+contactInfo[0]+' extended')
 
