9417727fdb4105af38d811f75776b9cb32e11a19
Tom <tcarrio@users.noreply.github.com>
2015-12-11 09:11:57 -0500

    Refactored to functions and checks __name__

--- extendwga.py
+++ extendwga.py
@@ -7,61 +7,24 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait 
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver import FirefoxProfile
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
-##################################################
-################ LOG THREAD ######################
-##################################################
-
 from collections import deque
 import threading
-
-
-active = 1
-log = deque([])
 
 def logStackListener():
 	while(active):
 		if len(log)>0:
 			print(log.popleft())
 
-t = threading.Thread(target=logStackListener)
-t.start()
-
-##################################################
-############## END LOG THREAD ####################
-##################################################
-
-login = []
-email = []
-IWT=5 # IMPLICIT WAIT TIME
-
-log.append('###Loading Selenium WebDriver###')
-
-# url = "https://visitorportal.benteler.net:8443/sponsorportal/"
-url = "https://visitorportal.benteler.net:8443/sponsorportal/PortalSetup.action?portal=ba7b1560-7c73-11e5-88ac-005056997abd"
-manage = "https://visitorportal.benteler.net:8443/sponsorportal/LoginSubmit.action?from=LOGIN#manageAccountsList"
-
-#browser = webdriver.Firefox()
-browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
-browser.set_window_size(1920,1080)
-# browser = webdriver.PhantomJS()
-browser.implicitly_wait(IWT)
-
-# '/root/dl/xwga/credentials'
-with open('credentials','r') as c:
-	login = c.read().strip().split('\n')
-	log.append('Credentials read')
-
-# '/root/dl/xwga/emails'
-with open('emails','r') as e:
-	emails = e.read().strip().split('\n')
-	log.append('Emails opened')
+def start_log():
+	active = 1
+	log = deque([])
+	t = threading.Thread(target=logStackListener)
+	t.start()
 
 def waitForPageById(element, seconds=15):
 	try:
@@ -130,21 +93,9 @@ def queryUser(contactInfo):
 		else:
 			acc_attempts+=1
 	
-	# search for user
-	# usernamefield = browser.find_elements_by_name('search')
-	# for u in usernamefield:
-	# 	print(u)
-	
-	# testing new username xpath
 	usernamefield = browser.find_element_by_xpath('//table[@class="search-container"]/tbody/tr/td/div/input')
 	browser.save_screenshot("username_field.png")
 	usernamefield.clear()
-
-	# try:
-	# 	username_clear= browser.find_element_by_xpath('//table[@class="search-container"]/tbody/tr/td/div/input/a')
-	# 	username_clear.click()
-	# except:
-	# 	log.append("No previous query to clear")
 
 	usernamefield.send_keys(contactInfo[0])
 	usernamefield.send_keys(Keys.RETURN)
@@ -160,7 +111,6 @@ def addUser(contactInfo):
 	time.sleep(2)
 
 	# retrieve form elements
-	#guesttype = browser.find_element_by_id("availableGuestTypes")
 	guesttype = browser.find_element_by_xpath('//div[@id="availableGuestTypes"]/div/div/div/select')
 	forenameForm = browser.find_element_by_name("firstName")
 	surnameForm = browser.find_element_by_name("lastName")
@@ -171,14 +121,13 @@ def addUser(contactInfo):
 	locationForm = browser.find_element_by_name("ui_optionaldata4_text_label")
 	validityForm = browser.find_element_by_name("days")
 	locationList = browser.find_element_by_name("location")
-	#locationList = browser.find_element_by_xpath('//span[@id="accessTimeContent"]/form/div/div/div/select')
+
 	ticketForm = browser.find_element_by_name("ui_optionaldata5_text_label")
 	
 	# set to 5 days
 	guesttype.click()
 	time.sleep(0.2)
 	guesttype.send_keys('5')
-	# browser.find_element_by_xpath('//option[@guest-type="9d981c21-7c8e-11e5-be52-005056997abd"]').click()
 	guesttype.send_keys(Keys.RETURN)
 
 	# fill out form
@@ -231,11 +180,11 @@ def createAccount(attempts=0):
 			time.sleep(2)
 			browser.find_element_by_partial_link_text("Create Accounts").click()
 			time.sleep(2)
-			createAccount(attempts+1)
+			return createAccount(attempts+1)
 	return 1
 
 def findUserInPage(contactInfo):
-	# usercheckbox = browser.find_element_by_xpath('//tr[@id="'+contactInfo[0]+'"]/td/input')
+
 	try:
 		searchedUser = browser.find_element_by_partial_link_text(contactInfo[0])
 		#searchedUser = browser.find_element_by_xpath('//table[@class="manage-accounts-table"]/tbody/tr/td[@title="'
@@ -278,49 +227,76 @@ def extendAccount():
 	confirmbtn.click()
 	return 1
 
-## Main Application
+def main():
+	login = []
+	email = []
+	IWT=5 # IMPLICIT WAIT TIME
+	
+	log.append('###Loading Selenium WebDriver###')
 
-if not loadManagementPortal():
+	# url = "https://visitorportal.benteler.net:8443/sponsorportal/"
+	url = "https://visitorportal.benteler.net:8443/sponsorportal/PortalSetup.action?portal=ba7b1560-7c73-11e5-88ac-005056997abd"
+	manage = "https://visitorportal.benteler.net:8443/sponsorportal/LoginSubmit.action?from=LOGIN#manageAccountsList"
+	
+	#browser = webdriver.Firefox()
+	browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+	browser.set_window_size(1920,1080)
+	# browser = webdriver.PhantomJS()
+	browser.implicitly_wait(IWT)
+	
+	# '/root/dl/xwga/credentials'
+	with open('credentials','r') as c:
+		login = c.read().strip().split('\n')
+		log.append('Credentials read')
+	
+	# '/root/dl/xwga/emails'
+	with open('emails','r') as e:
+		emails = e.read().strip().split('\n')
+		log.append('Emails opened')
+		
+	if not loadManagementPortal():
+		active = 0
+	log.append('Main portal logged in')
+	
+	time.sleep(1)
+	
+	for email in emails:
+	
+		if len(email) > 1:
+			contactInfo = email.split(';')
+		log.append('Contact info parsed for '+contactInfo[0])
+		if not isinstance(contactInfo, list):
+			log.append('Contact info incorrect')
+			continue
+	
+		time.sleep(1)
+	
+		if not queryUser(contactInfo):
+			continue
+		log.append('Queried for '+contactInfo[0])
+	
+		time.sleep(3)
+	
+		if not findUserInPage(contactInfo):
+			if not addUser(contactInfo):
+				log.append('User addition failed')
+		 		continue
+		 	log.append('User added to system')
+			continue
+		log.append('User found')	
+	
+		time.sleep(1)
+	
+		if not extendAccount():
+			log.append('Account extension for '+contactInfo[0]+' failed')
+			continue
+		log.append('Account '+contactInfo[0]+' extended')
+	
+	# exit browser
+	time.sleep(1)
 	active = 0
-log.append('Main portal logged in')
+	browser.quit()
 
-time.sleep(1)
-
-for email in emails:
-
-	if len(email) > 1:
-		contactInfo = email.split(';')
-	log.append('Contact info parsed for '+contactInfo[0])
-	if not isinstance(contactInfo, list):
-		log.append('Contact info incorrect')
-		continue
-
-	time.sleep(1)
-
-	if not queryUser(contactInfo):
-		continue
-	log.append('Queried for '+contactInfo[0])
-
-	time.sleep(3)
-
-	if not findUserInPage(contactInfo):
-		if not addUser(contactInfo):
-			log.append('User addition failed')
-	 		continue
-	 	log.append('User added to system')
-		continue
-	log.append('User found')	
-
-	time.sleep(1)
-
-	if not extendAccount():
-		log.append('Account extension for '+contactInfo[0]+' failed')
-		continue
-	log.append('Account '+contactInfo[0]+' extended')
-
-# exit browser
-time.sleep(1)
-active = 0
-browser.quit()
-
-exit()
+if __name__=="__main__":
+	start_log()
+	main()                                                                                                                                                         
